03afce0bf86a5e5e0d16108c9760ddd33a6197e5
liew jun young <liewjunyoung@gmail.com>
2022-05-09 09:40:04 +0800

    connected to firebase

--- app/src/main/java/com/example/andriod/maeassignment/MainActivity.kt
+++ app/src/main/java/com/example/andriod/maeassignment/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.andriod.maeassignment
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
